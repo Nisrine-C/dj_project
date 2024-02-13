46613c7e40c6a77bd810f21c42d44fc6bfc0ff52
Nisrine-C <nisrinechakir237@gmail.com>
2024-02-13 14:49:56 +0100

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
             <Route path="/">
               <Route index element={<Home />} />
             </Route>
+
           </Routes>
         </BrowserRouter>
       </div>
