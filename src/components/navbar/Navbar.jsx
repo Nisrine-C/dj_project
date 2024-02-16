0fd7697635669843f755274e2d3e908bd18ae5ef
Nisrine-C <nisrinechakir237@gmail.com>
2024-02-16 08:49:30 +0100

    header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,17 @@ import "./Navbar.css";
 const Navbar = () => {
   
     return (
-      <div className="Navbar">
-        <ul>
-            <li>home</li>
-        </ul>
+      <div className="navbar">
+        <a href="/" className="logo" >
+          <img src="./logo.svg" alt="logo"></img>
+          </a>
+        <nav>
+          <a className="nav_elem" href="/" >Home</a>
+          <a className="nav_elem" href="/calendar" >Calendar</a>
+          <a className="nav_elem" href="/pricing" >Pricing </a>
+          <a className="nav_elem" href="/contact" >Contact us</a>
+          <a className="nav_elem" href="/about" >About me</a>
+        </nav>
       </div>
     );
   };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,21 @@
+import "./Home.css"
+import SectionAboutArtist from "./components/Section_1/SectionAboutArtist";
+
 const Home = () => {
     return (
       <div className="Home">
-        <h1>h</h1>
+        <div className="hero">
+          <div className="background">
+            <img src="https://images.pexels.com/photos/2959192/pexels-photo-2959192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+          </div>
+          <div className="quote">
+            <span>I AM</span>
+            <p>PASSIONATE ABOUT DELIVERING PROFESSIONAL AND FUN ENTERTAINMENT ON YOUR WEDDING DAY!</p>
+          </div>
+        </div>
+        <div className="container">
+            <SectionAboutArtist/>
+        </div>
       </div>
     );
   };
