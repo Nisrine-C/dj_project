cdd682ca59809e1977dbc5f7831c05317ee81e0e
H3llDude <saadhell2459@gmail.com>
2024-02-16 03:24:05 +0100

    commit num 1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,24 @@
+import "./Home.css";
 const Home = () => {
-    return (
-      <div className="Home">
-        <h1>h</h1>
+  return (
+    <div className="Home">
+      <div className="CTA-Section">
+        <div className="CTA-intro">
+          <div className="CTA-head">Lorem ipsum dolor sit.</div>
+          <div className="CTA-pg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+            iusto aliquam cumque laborum dolorum molestiae unde odit odio eius
+            dolore esse beatae, sed alias sint quisquam inventore accusamus,
+            facilis maiores sunt neque culpa, libero quam similique. Odit,
+            sapiente temporibus?.
+          </div>
+        </div>
+        <div className="CTA-SideVideo">
+          <div className="CTA-Video"></div>
+        </div>
       </div>
-    );
-  };
-  
-  export default Home;
+    </div>
+  );
+};
+
+export default Home;
