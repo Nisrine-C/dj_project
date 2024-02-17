174265b441e34595136a72abb07bfe17457aaca6
Nisrine-C <nisrinechakir237@gmail.com>
2024-02-17 17:11:53 +0100

    removed padding

--- src/pages/home/components/Section_1/SectionAboutArtist.jsx
+++ src/pages/home/components/Section_1/SectionAboutArtist.jsx
@@ -5,12 +5,12 @@ const SectionAboutArtist = () => {
     return (
       <div className="section-about-artist">
         <div className="images_1">
-          <img className="img_1" src="" alt="image"></img>
-          <img className="img_2" src="" alt="image"></img>
+          <div className="img_1" ></div>
+          <div className="img_2" ></div>
         </div>
         <div className="paragraph">
-          <h6 className="Title"></h6>
-          <p></p>
+          <h6 className="Title">Hi I am</h6>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non feugiat libero, ac dapibus magna. Maecenas scelerisque magna ut vehicula accumsan. Nullam. Proin pulvinar vehicula mi, a faucibus risus rutrum quis. Sed non ligula quis eros pulvinar placerat vel vel elit. Aenean ac luctus leo, vitae interdum erat. Etiam ullamcorper non quam in sagittis.</p>
         </div>
         <div className="images_2">
           
