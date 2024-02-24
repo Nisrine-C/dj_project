c8e8cd7350d8b2ab33b26c510a125291b9ad5ba6
Nisrine-C <nisrinechakir237@gmail.com>
2024-02-24 15:11:42 +0100

    image fixed

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -2,6 +2,7 @@ import "./Booking.css";
 import Tuto from "../../components/tuto/Tuto";
 import Calendar from "../../components/calendar/Calendar";
 import BookingForm from "../../components/booking-form/BookingForm";
+import Image from "../../assets/pexels-tara-winstead-647955.jpg";
 
 
 
@@ -9,6 +10,7 @@ const Booking = () => {
   return (
     <div className="Booking" >
       <div className="title-container">
+        <img src={Image} />
         <h1>Book your DJ</h1>
       </div>
       <div className="tutorials">
